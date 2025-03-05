996695db9b49613c5500fc93d3f8ac2b6b240439
eschloss7 <emmy.schlossberg@colostate.edu>
2025-03-04 20:18:52 -0700

    Public keyword

--- Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
+++ Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -27,12 +27,12 @@ public class LootAngle
 {
     int angle;
     int step;
-    LootAngle(int increment)
+    public LootAngle(int increment)
 {
     step = increment;
     angle = 0;
 }
-     int NextAngle()
+     public int NextAngle()
     {
         int currentAngle = angle;
         angle = Helpers.WrapAngle(angle + step);
